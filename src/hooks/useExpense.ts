ddccb2e56ca2d1b5c52bb3c3ba1c0fa3a57a461b
Wendellynizer <wendelsabelo27@gmail.com>
2025-08-24 20:50:26 +0800

    fixed dependecies

--- src/hooks/useExpense.ts
+++ src/hooks/useExpense.ts
@@ -53,7 +53,7 @@ export const useExpenses = () => {
       const stored = localStorage.getItem(`expenses_${address}`);
       const storedExpenses = stored ? JSON.parse(stored) : mockExpenses;
       
-      setExpenses(storedExpenses.map((e: any) => ({
+      setExpenses(storedExpenses.map((e) => ({
         ...e,
         date: new Date(e.date)
       })));
